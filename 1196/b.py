4eb186adc76ff8ba5759008e41f78e9d4c6efe4b
Alex Buchkovsky <alex.buchkovsky@gmail.com>
2019-07-28 16:50:23 +0300

    fix b task

--- 1196/b.py
+++ 1196/b.py
@@ -1,17 +1,25 @@
 q = int(input())
 for i in range(q):
     n, k = map(int, input().split())
-    a = list(map(int, input().split()))
-    ans = []
-    s = 0
-    for j in range(n):
-        s += a[j]
-        if s % 2 and k > 1:
-            ans.append(j+1)
-            k -= 1
-            s = 0
-    if s % 2 and k == 1:
-        print('YES')
-        print(*ans, n)
+    if n == 1:
+        t = int(input())
+        if t % 2 == 1:
+            print('YES')
+            print(1)
+        else:
+            print('NO')
     else:
-        print('NO')
+        a = list(map(int, input().split()))
+        ans = []
+        s = 0
+        for j in range(n):
+            s += a[j]
+            if s % 2 and k > 1:
+                ans.append(j+1)
+                k -= 1
+                s = 0
+        if s % 2 and k == 1:
+            print('YES')
+            print(*ans, n)
+        else:
+            print('NO')
