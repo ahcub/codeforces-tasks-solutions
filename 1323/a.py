5d14c04681f24415a1cb498a315f76e04ffc4a8c
Alex Buchkovsky <alex.buchkovsky@gmail.com>
2020-03-14 17:40:21 +0200

    fix solution

--- 1323/a.py
+++ 1323/a.py
@@ -4,12 +4,14 @@ for _ in range(int(input())):
     left = None
     for i, el in enumerate(a, 1):
         if el % 2 == 0:
+            print(1)
             print(i)
             break
         else:
             if left is None:
                 left = i
             else:
+                print(2)
                 print(left, i)
                 break
     else:
